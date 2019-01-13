950642a14588a10a423b8e2e4bd0dbdd73c5fb96
Marcin Lipok <mcnlpk@gmail.com>
2019-01-13 18:07:45 +0100

    działające dodawanie do listy

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var formularz = document.getElementById("formularz");
 var pole = document.getElementById("pole");
+var lista = document.getElementById("lista");
+
+function stworzElemntListy (zadanie) {
+    var element = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    element.innerText = zadanie;
+    element.prepend(checkbox);
+
+    return element;
+}
 
 formularz.onsubmit = function (event) {
     event.preventDefault();
     var zadanie = pole.value;
     pole.value = "";
-    console.log(zadanie);
+    var element = stworzElemntListy(zadanie);
+    lista.prepend(element);
 }
