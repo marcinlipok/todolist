7f36cf14c8df8276a090aa6913ba88a37262517b
Marcin Lipok <mcnlpk@gmail.com>
2019-01-13 18:28:58 +0100

    zmiana koloru tła po kliknięciu

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function stworzElemntListy (zadanie) {
     var element = document.createElement("li");
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.onclick = checkboxKlikniety;
     element.innerText = zadanie;
     element.prepend(checkbox);
 
     return element;
+}
+
+function checkboxKlikniety (event) {
+    var element = event.target.parentElement;
+    element.classList.toggle("ukonczony");
 }
 
 formularz.onsubmit = function (event) {
